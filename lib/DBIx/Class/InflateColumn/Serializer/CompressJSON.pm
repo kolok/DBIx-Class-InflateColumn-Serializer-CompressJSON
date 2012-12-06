d34e19a11c6f0e15c0b46ee24304441ec2c07962
Nicolas Oudard <noudard@weborama.com>
2012-12-06 18:45:58 +0100

    update version to 0.003

--- lib/DBIx/Class/InflateColumn/Serializer/CompressJSON.pm
+++ lib/DBIx/Class/InflateColumn/Serializer/CompressJSON.pm
@@ -6,7 +6,7 @@
 #
 package DBIx::Class::InflateColumn::Serializer::CompressJSON;
 {
-  $DBIx::Class::InflateColumn::Serializer::CompressJSON::VERSION = '0.002';
+  $DBIx::Class::InflateColumn::Serializer::CompressJSON::VERSION = '0.003';
 }
 
 # ABSTRACT: DBIx::Class::InflateColumn::Serializer::CompressJSON - JSON compressed Inflator
